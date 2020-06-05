b11afc01fe71551376ade060520d99d773827073
d4nilyuk <d4nilyuk@gmail.com>
2020-06-05 17:31:12 +1000

    test fucntion, views, models, migrations

--- myfirst/myfirst/apps/schoolapp/models.py
+++ myfirst/myfirst/apps/schoolapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField('Teacher First name', max_length = 100)
+    last_name = models.CharField('Teacher Last name', max_length = 100)
+    hourlyRate = models.IntegerField
+
+class schoolClass(models.Model):
+    class_id = models.IntegerField
+
+class Student(models.Model):
+    schoolClass = models.ForeignKey(schoolClass, on_delete = models.CASCADE)
+    first_name = models.CharField('Student First name', max_length = 100)
+    last_name = models.CharField('Student Last name', max_length = 100)
+    enrolNum = models.IntegerField
+    details = models.TextField('Student Details')
+    attendence = models.BooleanField
+
--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'schoolapp.apps.SchoolappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
