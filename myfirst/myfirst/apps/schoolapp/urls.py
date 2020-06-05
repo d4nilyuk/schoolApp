cf398f7977e7cc5a99a82c1082b91f067c44b6d1
d4nilyuk <d4nilyuk@gmail.com>
2020-06-06 06:08:57 +1000

    pages students, classes, teachers shows data

--- myfirst/myfirst/apps/schoolapp/urls.py
+++ myfirst/myfirst/apps/schoolapp/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 app_name = 'schoolapp'
 urlpatterns = [
-    path('students', views.index, name = 'student'),
-    path('students/<int:student_id>/', views.student_details, name = 'student_details')
+    path('students', views.students, name = 'student'),
+    path('students/<int:student_id>/', views.student_details, name = 'student_details'),
+
+    path('teachers', views.teachers, name = 'teacher'),
+    path('teachers/<int:teacher_id>/', views.teacher_details, name = 'teacher_details'),
+
+    path('schoolclasses', views.schoolclasses, name = 'schoolclass'),
+    path('schoolclasses/<int:schoolclass_id>/', views.schoolclass_details, name = 'schoolclass_details')
 ]
--- myfirst/myfirst/apps/schoolapp/views.py
+++ myfirst/myfirst/apps/schoolapp/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 from.models import Student, Teacher, SchoolClass
 
-def index(request):
+def students(request):
     student = Student.objects.order_by('student_id')
     return render(request, 'students/students.html', {'student': student})
+
+def teachers(request):
+    teacher = Teacher.objects.order_by('teacher_id')
+    return render(request, 'teachers/teachers.html', {'teacher': teacher})
+    
+def schoolclasses(request):
+    schoolclass = SchoolClass.objects.order_by('schoolclass_id')
+    return render(request, 'schoolclasses/schoolclasses.html', {'schoolclass': schoolclass})
 
 def student_details(request, student_id):
     try:
@@ -14,3 +22,19 @@ def student_details(request, student_id):
         raise Http404("Student is not found")
     
     return render(request, 'students/students.html', {'student': a}) 
+
+def teacher_details(request, teacher_id):
+    try:
+        a = Teacher.objects.get(id = teacher_id)
+    except:
+        raise Http404("Teacher is not found")
+    
+    return render(request, 'teachers/teachers.html', {'teacher': a}) 
+
+def schoolclass_details(request, schoolclass_id):
+    try:
+        a = SchoolClass.objects.get(id = schoolclass_id)
+    except:
+        raise Http404("Class is not found")
+    
+    return render(request, 'schoolclasses/schoolclasses.html', {'schoolclass': a}) 
