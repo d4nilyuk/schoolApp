901dfae8a2a0038c69b4a01c1b41f11fc08f34d4
d4nilyuk <d4nilyuk@gmail.com>
2020-06-05 18:20:24 +1000

    super admin config

--- myfirst/myfirst/apps/schoolapp/admin.py
+++ myfirst/myfirst/apps/schoolapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SchoolClass, Teacher, Student
+
+admin.site.register(SchoolClass)
+admin.site.register(Teacher)
+admin.site.register(Student)
--- myfirst/myfirst/apps/schoolapp/models.py
+++ myfirst/myfirst/apps/schoolapp/models.py
@@ -5,16 +5,25 @@ from django.db import models
 class Teacher(models.Model):
     first_name = models.CharField('Teacher First name', max_length = 100)
     last_name = models.CharField('Teacher Last name', max_length = 100)
-    hourlyRate = models.IntegerField
+    hourlyRate = models.IntegerField()
+
+    def __str__(self):
+        return self.last_name
 
 class SchoolClass(models.Model):
-    class_id = models.IntegerField
+    class_id = models.IntegerField()
+
+    def __str__(self):
+        return self.class_id
 
 class Student(models.Model):
     schoolClass = models.ForeignKey(SchoolClass, on_delete = models.CASCADE)
     first_name = models.CharField('Student First name', max_length = 100)
     last_name = models.CharField('Student Last name', max_length = 100)
-    enrolNum = models.IntegerField
+    enrolNum = models.IntegerField()
     details = models.TextField('Student Details')
-    attendence = models.BooleanField
+    attendence = models.BooleanField()
+
+    def __str__(self):
+        return self.schoolClass
 
